73256f5af11df5a994eaf1357256b07c6e63782e
T-Hiropy <akidokimee53@yahoo.co.jp>
2025-03-17 00:38:10 +0900

    add kadai_010

---
+++ kadai_010/summary.js
@@ -0,0 +1,22 @@
+$(function(){
+  // change-color
+  $('#change-color').on('click', function(){
+    $('#target').css('color', 'Red');
+  });
+ 
+  // change-text
+  $('#change-text').on('click', function(){
+    $('#target').text('Hello!');
+  });
+
+  // fade-out
+  $('#fade-out').on('click', function(){
+    $('#target').fadeOut();
+  });
+
+  // fade-in
+  $('#fade-in').on('click', function(){
+    $('#target').fadeIn();
+  });
+
+});
